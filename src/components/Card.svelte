701dbfb3058909f6ddf2c216a108c184effa9012
zcochran <fuzzylimes@gmail.com>
2022-05-22 12:40:57 -0400

    Small style updates + GachAni

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,7 +1,7 @@
 <script>
-    import Tag from './Tag.svelte';
+  import Tag from "./Tag.svelte";
 
-    export let project;
+  export let project;
 </script>
 
 <div class="card">
@@ -12,37 +12,40 @@
   </header>
   <div class="card-image">
     <figure class="image is-4by3">
-        {#if project.image !== ''}
-      <img src={project.image} alt="Project example">
+      {#if project.image !== ""}
+        <img src={project.image} alt="Project example" />
       {:else}
-      <img src="./images/default.jpg" alt="No preview available">
+        <img src="./images/default.jpg" alt="No preview available" />
       {/if}
     </figure>
   </div>
   <div class="card-content">
     <div class="media">
       <div class="media-content">
-        <p class="title is-4 is-link"><a href={project.url}>Visit Site</a> | <a href={project.github}>github</a></p>
+        <p class="title is-4 is-link">
+          <a href={project.url}>Visit Site</a> |
+          <a href={project.github}>github</a>
+        </p>
       </div>
     </div>
 
     <div class="content">
       {project.description}
-      <br><br>
+      <br /><br />
       <span>{project.completed}</span>
-      <br><br>
+      <br /><br />
       {#each project.tags as tag}
-        <Tag tag={tag}/>
+        <Tag {tag} />
       {/each}
     </div>
   </div>
 </div>
 
 <style>
-    .card {
-        background-color: #F2F1EF;
-        /* height: 100%;
-        display: flex;
-        flex-direction: column; */
-    }
+  .card {
+    background-color: #f2f1ef;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 </style>
